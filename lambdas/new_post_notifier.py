549c59dc3be6ef07e82fe8f5eec94515ecaf2b6c
Kai Stullich <kairstullich@gmail.com>
2020-07-19 12:09:50 -0400

    added documentation

--- lambdas/new_post_notifier.py
+++ lambdas/new_post_notifier.py
@@ -8,6 +8,14 @@ sns = boto3.client('sns', region_name='us-east-1')
 
 
 def get_post_from_db(post_id):
+    """ Get url, title, and post_category for the ``post_id``
+
+    Args:
+        post_id (str): post ID to lookup
+
+    Returns:
+        dict: API call response
+    """
     return dynamo.get_item(
         TableName=DYNAMO_TABLE,
         Key={'post_id': {'S': post_id}},
@@ -16,6 +24,14 @@ def get_post_from_db(post_id):
 
 
 def send_sns_notification(post_attrs):
+    """ Publish message to the corresponding posts SNS topic
+
+    Args:
+        post_attrs (dict): the API call response from ``get_post_from_db()``
+
+    Returns:
+        dict: SNS API response (has the message-id)
+    """
     post = post_attrs['Item']
     post_categor = post['post_category']['S']
     title = post['title']['S']
@@ -28,6 +44,14 @@ def send_sns_notification(post_attrs):
 
 
 def update_notified_attr(post_id):
+    """ Update "notified" DB attribute for the ``post_id``
+
+    Args:
+        post_id (str): post ID to edit
+
+    Returns:
+        dict: AWS Dynamo API call response
+    """
     return dynamo.update_item(
         TableName=DYNAMO_TABLE,
         Key={'post_id': {'S': post_id}},
@@ -36,8 +60,18 @@ def update_notified_attr(post_id):
 
 
 def lambda_handler(event, context):
+    """ AWS lambda handler main function
+
+    Args:
+        event (dict): payload response from the "gundeals-reddit-new-post-collector" lambda
+        context ():
+
+    Returns:
+        None
+    """
     lambda_payload = event.get('responsePayload')
     if lambda_payload['newPostsInserted']:
         for _, post_id_list in lambda_payload['newPostsProcessed'].items():
             for post_id in post_id_list:
                 send_sns_notification(get_post_from_db(post_id))
+                update_notified_attr(post_id)
